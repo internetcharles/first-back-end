e47d3bcf0b544bfbb85186e45b56bac7f56cfde7
internetcharles <charlesodellsmith@gmail.com>
2020-08-11 15:07:56 -0700

    everything work

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { geoData } = require('./data/geo.js');
-const { weatherData } = require('./data/weather.js');
 const request = require('superagent')
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,13 +13,61 @@ app.use(cors());
 
 app.use(express.static('public'));
 
-function getWeather(lat, lon) {
-    const data = weatherData.data;
+async function getWeather(lat, lon) {
+    const response = await request.get(`https://api.weatherbit.io/v2.0/forecast/daily?&lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`)
 
-    const forecastArray = data.map((weatherItem => {    
+    const weatherData = response.body.data;
+
+    const forecastArray = weatherData.map((weatherItem => {    
         return {
         forecast: weatherItem.weather.description,
         time: new Date(weatherItem.ts * 1000),
+    };
+}))
+
+    return forecastArray;
+}
+
+async function getHikes(lat, lon) {
+    const response = await request.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=200&key=${process.env.HIKING_API_KEY}`)
+
+    const trailData = response.body.trails;
+
+
+
+    const trailArray = trailData.map((trail => {
+        
+        return {
+            name: trail.name,
+            stars: trail.stars,
+            location: trail.location,
+            length: trail.length,
+            star_votes: trail.starVotes,
+            summary: trail.summary,
+            trail_url: trail.url,
+            condition: trail.conditionStatus,
+            condition_date: trail.conditionDate.split(' ')[0],
+            condition_time: trail.conditionDate.split(' ')[1]
+        }
+    }))
+    
+    return trailArray;
+
+}
+
+async function getReview(lat, lon) {
+    const response = await request.get(`https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lon}`)
+    .set('Authorization', 'Bearer 2Nu7g-AYANIpmT-HcwoeZz7oW89a_bHZRy9FnxlcqWaJ_NRFWiC3lw18EPlK-LeiWYQ5jH2MfNFVRDlGD6mIDR58WCC0REz5Ta_TiLu0UfTwm92YG9Z5ddhEIMEyX3Yx');
+
+    const reviewData = response.body.businesses;
+
+    const forecastArray = reviewData.map((review => {    
+        return {
+            name: review.name,
+            image_url: review.image_url,
+            price: review.price,
+            rating: review.rating,
+            url: review.url
     };
 }))
 
@@ -49,20 +95,48 @@ app.get('/location', async(req, res) => {
     res.json(mungedData);
   });
 
-  app.get('/weather', (req, res) => {
+  app.get('/weather', async(req, res) => {
     try {
-
         const userLat = req.query.latitude;
         const userLon = req.query.longitude;
 
-        const mungedData = getWeather(userLat, userLon);
+        const mungedData = await getWeather(userLat, userLon);
 
         res.json(mungedData);
     }
-    catch{
-
+    catch (e) {
+        res.status(500).json({ error: e.message })
     }
     });
+
+    app.get('/trails', async(req, res) => {
+        try {
+            const userLat = req.query.latitude;
+            const userLon = req.query.longitude;
+    
+            const mungedData = await getHikes(userLat, userLon);
+    
+            res.json(mungedData);
+        }
+        catch (e) {
+            res.status(500).json({ error: e.message })
+        }
+        });
+
+    app.get('/reviews', async(req, res) => {
+        try {
+            const userLat = req.query.latitude;
+            const userLon = req.query.longitude;
+        
+            const mungedData = await getReview(userLat, userLon);
+        
+            res.json(mungedData);
+        }
+        catch (e) {
+            res.status(500).json({ error: e.message })
+        }
+        })        
+        
 
 
 app.listen(port, () => {
