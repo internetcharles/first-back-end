5d67ad2809a65bf4604f8d7662192226c8bc3052
internetcharles <charlesodellsmith@gmail.com>
2020-08-11 09:58:32 -0700

    add process.env

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { geoData } = require('./data/geo.js');
 const { weatherData } = require('./data/weather.js');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
